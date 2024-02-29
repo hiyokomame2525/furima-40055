32c2dbaa5fcc51160be64cdf0e3f5dc89ade2ada
hiyokomame2525 <5656mamesiba@gmail.com>
2024-02-29 22:05:51 +0900

    訂正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
     def move_to_index
       unless user_signed_in?
-        redirect_to  new_user_registration
+        redirect_to   new_user_session_path
       end
     end 
 
